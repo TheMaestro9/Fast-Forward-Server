0a0468ea16728cb323d25c6e7ba89234f122f80e
nadinetarek <nadine.tarek19@gmail.com>
2017-10-14 00:39:12 +0200

    sending email to accepted

--- page_modules/applicantsServer.js
+++ page_modules/applicantsServer.js
@@ -17,9 +17,7 @@
           {
             if (result[i].status==="accepted"){
               toSend.accepted.push(result[i])
-              GHF = require("./GlobalHelperFunctions") ; 
-              console.log("going to send acceptance email") ;
-          //    GHF.SendAnEmail(UserEmail ,'Acceptance Notification',"Congratulation you got accepted") ; 
+            
             }
               
             else
@@ -36,8 +34,21 @@
   exports.AcceptApplicant  = function (connection , response , price , userID , simDateId ) { 
 
     var stat = ""  ; 
-    if (price === 0)
+    if (price === 0){
       stat = "accepted" ; 
+      email = "select user_email from user where user_id ="+userID; 
+       connection.query(email , function (err, result) {
+        if (err) {
+          console.log(err);
+         response.status(500).send(err);
+        } else {
+          GHF = require("./GlobalHelperFunctions") ; 
+          GHF.SendAnEmail(result[0].user_email ,'Acceptance Notification',"Congratulations you got accepted in the simulation.") ; 
+          // response.send(result);
+            }
+             
+      });
+    }
     else
       stat = "pending payment"; 
 
