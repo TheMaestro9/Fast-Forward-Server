a6d313b68f824e5f94b1c68a5e080214765c1885
nadinetarek <nadine.tarek19@gmail.com>
2017-11-18 15:22:26 +0200

    password encryption changes

--- page_modules/loginServer.js
+++ page_modules/loginServer.js
@@ -1,3 +1,4 @@
+var CryptoJS = require("crypto-js");
 exports.Login  = function (connection , response , UserEmail ,password) { 
 
      var toSend = {
@@ -37,6 +38,7 @@ exports.ForgotPassword  = function (connection , response , UserEmail ) {
       "msg" : "" 
     } ; 
     console.log("INNNNNNNNN") ; 
+   
     // execute a query on our database
     getUserPassword( connection , response , UserEmail ,  function (result) {
     
@@ -50,14 +52,39 @@ exports.ForgotPassword  = function (connection , response , UserEmail ) {
         else {
           GHF = require("./GlobalHelperFunctions") ; 
           console.log("going to send the email") ;
-          GHF.SendAnEmail(UserEmail ,'Change Password',"Your password is "+result[0].password) ; 
-          toSend.result= true ;
-          toSend.msg  = "An Email Has Been Sent To You With your Password Check It Out!" ;
+          var qstring = "select user_id from user where user_email ='"+UserEmail+"'"; 
+          console.log("the query: "+qstring +"\n"); 
+          connection.query(qstring , function (err, r) {
+            if (err) {
+              console.log(err);
+             response.status(500).send(err);
+            } else {
+              console.log("RRRR",r[0].user_id)
+              GHF.SendAnEmail(UserEmail ,'Change Password','http://ffserver.eu-gb.mybluemix.net/forget1?user_id='+r[0].user_id) ; 
+              toSend.result= true ;
+              toSend.msg  = "An Email Has Been Sent To You With your Password Check It Out!" ;
+            }
+          
+          });
+         
           response.send (toSend);  
         }
       
 
     });
+}
+function getUserID(connection,response,userEmail,callback){
+  var qstring = "select user_id from user where user_email ='"+userEmail+"'"; 
+  console.log("the query: "+qstring +"\n"); 
+  connection.query(qstring , function (err, result) {
+    if (err) {
+      console.log(err);
+     response.status(500).send(err);
+    } else {
+      callback(result) ; 
+    }
+  
+  });
 }
 
 function getUserPassword (connection , response , userEmail , callback){
@@ -76,3 +103,81 @@ function getUserPassword (connection , response , userEmail , callback){
 
 }
 
+exports.encryptAllPasswords=function(connection,response){
+  connection.query('SELECT password FROM user', function(err,result) {
+    if (err) {
+      console.log(err);
+     response.status(500).send(err);
+    } else {
+      console.log('Password: ', result.length);
+      for(i=0;i<result.length;i++)
+      {
+        console.log('Password: ',i, result[i].password)
+        var wordArray = CryptoJS.enc.Utf8.parse(result[i].password);
+        var base64 = CryptoJS.enc.Base64.stringify(wordArray);
+        console.log('encrypted:', base64);
+        var qstring = "update user set password='"+base64+ "' where user_id="+(i+1);
+        connection.query(qstring , function (err, result) {
+        if (err) {
+        console.log(err.message); 
+        response.send(err) ;
+        } else {
+         console.log("PASSS ENCRYPTEDDDD")
+        }
+          });
+      };
+    }
+  
+  });
+}
+
+exports.LoginAfterEncryption  = function (connection , response , UserEmail ,password) { 
+     var wordArray = CryptoJS.enc.Utf8.parse(password);
+     var base64 = CryptoJS.enc.Base64.stringify(wordArray);
+     console.log('encrypted:', base64);
+       var toSend = {
+        "result" : false  
+      } ; 
+      // execute a query on our database
+      var qstring = "select user_name , user_id , company_or_not from user where user_email ='"+UserEmail+"' and password='"+base64 +"'"; 
+      console.log("the query: "+qstring +"\n"); 
+      connection.query(qstring , function (err, result) {
+        if (err) {
+          console.log(err);
+         response.status(500).send(err);
+        } else {
+          if (result.length === 0)
+          {
+           response.send(toSend); 
+          }
+          else {
+  
+         //   checkExpo (result[0].user_id , function(reso){
+  
+                toSend.result= true ;
+           //     toSend  = Object.assign(toSend , reso) ;
+                toSend  = Object.assign(toSend , result[0]) ;
+                response.send (toSend);  
+          //  }) ; 
+            
+          }
+        }
+  
+      });
+  }
+
+exports.changePassword=function(connection,request,response) {
+  console.log("REQ PASS",request.body.pwd);
+  var wordArray = CryptoJS.enc.Utf8.parse(request.body.pwd);
+  var base64 = CryptoJS.enc.Base64.stringify(wordArray);
+  console.log('encrypted:', base64);
+  var qstring = "update user set password='"+base64+ "' where user_id=4";
+connection.query(qstring , function (err, result) {
+if (err) {
+console.log(err.message); 
+response.send(err) ;
+} else {
+console.log("changed Password successfully");
+}
+});
+};
